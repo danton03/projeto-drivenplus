8a07cc76bc9d5cc9cbf5e82cc22dbd650c5bf4be
Danton Matheus <dantoncosta03@hotmail.com>
2022-06-01 23:11:41 -0300

    teste na tela de login

--- src/components/TelaLogin.jsx
+++ src/components/TelaLogin.jsx
@@ -1,9 +1,17 @@
 import { ContainerPage } from "./layouts/ContainerPage"
+import { Conteudo } from "./layouts/TelaLoginStyles";
 
 export default function TelaLogin() {
   return(
     <ContainerPage>
-      <h1 style={{color: "white"}}>Eu sou a tela de Login</h1>
+      <Conteudo>
+        <img src="/assets/images/drivenplus_logo.svg" alt="Logo da Driven" />
+        <form>
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="Senha" />
+          <button type="submit">Entrar</button>
+        </form>
+      </Conteudo>
     </ContainerPage>
   );
 }
--- src/components/layouts/TelaLoginStyles.js
+++ src/components/layouts/TelaLoginStyles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 const Conteudo = styled.div`
-  &, form { display: flex;
+  &, form { 
+  display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
@@ -18,29 +19,36 @@ const Conteudo = styled.div`
 
   form{
     gap: 16px;
+
+    input{
+      width: 100%;
+      max-width: 298px;
+      height: 52px;
+      border: none;
+      border-radius: 8px;
+  
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 16px;
+      color: var(--cor-texto-input);
+    }
+  
+    button{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  
+      width: 100%;
+      max-width: 298px;
+      height: 52px;
+      
+      color: var(--cor-branca);
+      background: var(--cor-rosa);
+      border-radius: 8px;
+      border: none;
+    }
   }
 
-  input{
-    border-radius: 8px;
-
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 16px;
-    color: var(--cor-texto-input);
-  }
-
-  button{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    width: 100%;
-    max-width: 298px;
-    height: 52px;
-
-    background: var(--cor-rosa);
-    border-radius: 8px;
-  }
 `;
 
 export {Conteudo};
